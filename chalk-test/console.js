a21a2bbe5b69abb2a1bc6191c620d22b02260f66
TalArbatov <talarbatov98@gmail.com>
2018-07-26 09:11:39 +0300

    chalk interface basic options

---
+++ chalk-test/console.js
@@ -0,0 +1,5 @@
+const c = (text) => {console.log(text)}
+
+module.exports = {
+    c:c
+}
---
+++ chalk-test/index.js
@@ -0,0 +1,24 @@
+const chalk = require('chalk');
+const c = require('./console').c;
+
+
+//const log = (text) => {console.log(text)}
+
+const console = {
+    log: (text) => {c(text)},
+    warn: (text) => {c(chalk.yellow(text))},
+    error: (text) => {c(chalk.red(text))},
+    success: (text) => {c(chalk.green(text))},
+
+
+    red: (text) => {c(chalk.red(text))},
+    green: (text) => {c(chalk.green(text))},
+    yellow: (text) => {c(chalk.yellow(text))},
+    blue: (text) => {c(chalk.blue(text))},
+    cyan: (text) => {c(chalk.cyan(text))},
+    white: (text) => {c(chalk.white(text))},
+    grey: (text) => {c(chalk.grey(text))},
+    black: (text) => {c(chalk.black(text))},
+}
+
+module.exports = console;
